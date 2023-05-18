20d67dfd65de25d083d1bbf2502fd78091cb7fae
rauda08 <raudajnnh08@gmail.com>
2023-05-18 18:50:23 +0700

    mengembalikan nilai

--- 113_singlelinkedlist/113_singlelinkedlist.cpp
+++ 113_singlelinkedlist/113_singlelinkedlist.cpp
@@ -38,3 +38,6 @@ void addNode() {
     while ((current != NULL) && (nim >= current->noMhs))
     {
         if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl; 
+            return;
