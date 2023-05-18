2db0386376ffae577d1c5624ea86fa5268dc9a39
rauda08 <raudajnnh08@gmail.com>
2023-05-18 18:58:10 +0700

    pengembalian bool dan melakukan perulangan

--- 113_singlelinkedlist/113_singlelinkedlist.cpp
+++ 113_singlelinkedlist/113_singlelinkedlist.cpp
@@ -39,5 +39,21 @@ void addNode() {
     {
         if (nim == current->noMhs)
         {
-            cout << "NIM sudah ada" << endl; 
+            cout << "NIM sudah ada" << endl;
             return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
+bool serachNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current == START;
+    while (current != NULL && nim > current->noMhs)
+    {
+    }
+}
