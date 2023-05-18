bfc1378dc944f9612a3e628cff059a64cd86056f
rauda08 <raudajnnh08@gmail.com>
2023-05-18 22:04:04 +0700

    menambahkan case 4

--- 113_singlelinkedlist/113_singlelinkedlist.cpp
+++ 113_singlelinkedlist/113_singlelinkedlist.cpp
@@ -174,6 +174,9 @@ int main() {
             case 3:
                 traverse();
                 break;
+            case 4:
+                searchData();
+                break;
 
                 int nim;
 
